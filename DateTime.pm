0afdad04f57d09705b3de3df474f2886c9d7639b
Eugene van der Pijll <pijll@gmx.net>
2003-04-06 12:06:33 +0000

    Import of PIJLL/Bundle-DateTime-0.01 from CPAN.
    gitpan-cpan-distribution: Bundle-DateTime
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         PIJLL/Bundle-DateTime-0.01.tar.gz
    gitpan-cpan-author:       PIJLL
    gitpan-cpan-maturity:     released

---
+++ DateTime.pm
@@ -0,0 +1,69 @@
+package Bundle::DateTime;
+
+$VERSION = 0.01;
+
+1;
+
+__END__
+
+=head1 NAME
+
+Bundle::DateTime - Bundle with all DateTime related modules
+
+=head1 SYNOPSIS
+
+C<perl -MCPAN -e 'install Bundle::DateTime'>
+
+=head1 CONTENTS
+
+DateTime
+
+DateTime::TimeZone - should be installed by DateTime
+
+DateTime::Set
+
+DateTime::Format::Baby
+
+DateTime::Format::Builder
+
+DateTime::Format::Excel
+
+DateTime::Format::IBeat
+
+DateTime::Format::ICal
+
+DateTime::Format::Mail
+
+DateTime::Format::MySQL
+
+DateTime::Format::W3CDTF
+
+DateTime::Calendar::Christian
+
+DateTime::Calendar::Julian
+
+DateTime::Calendar::Mayan
+
+DateTime::Calendar::Pataphysical
+
+DateTime::Event::Easter
+
+DateTime::Astro::Sunrise - development release
+
+=head1 DESCRIPTION
+
+This bundle contains all modules related to the DateTime project: base
+modules, formatters and parsers, calendars and event modules.
+
+=head1 AUTHOR
+
+Eugene van der Pijll <pijll@gmx.net>
+
+The best address to send questions or additions to is the datetime
+mailing list <datetime@perl.org>.
+
+=head1 SEE ALSO
+
+http://datetime.perl.org/
+
+=cut
